65bbeee2f1d7d2e4b41c6cededeca0478189fb24
Jeff R. Allen <jra@nella.org>
2012-04-12 01:36:25 +0200

    Make is playable.

--- cmd/mapscroll/mapscroll.go
+++ cmd/mapscroll/mapscroll.go
@@ -1,27 +1,41 @@
-// Copyright 2011 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package main
 
 import (
 	"code.google.com/p/x-go-binding/ui"
 	"code.google.com/p/x-go-binding/ui/x11"
+	"flag"
 	"fmt"
 	"image"
+	"image/draw"
 	"image/jpeg"
 	"image/png"
-	"image/draw"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
+
+var game *bool = flag.Bool("game", false, "make it playable")
+
+var rocketPos int
+var rocketLimit int
+var origin image.Point
+
+const tileHeight = 256
+const tileWidth = 256
+
+type flame struct {
+	active bool
+	where  image.Point
+}
+
+var flames [10]flame
 
 func imagePuller(urls chan string, imgs chan *image.Image) {
 	for url := range urls {
 		r, err := http.Get(url)
 		if err == nil {
-			log.Print("Fetched ", url)
+			//log.Print("Fetched ", url)
 			ctype, found := r.Header["Content-Type"]
 			if found {
 				switch {
@@ -63,16 +77,75 @@ func urlGen(urls chan string) {
 	}
 }
 
+//2012/04/11 23:31:13 event: {65362}	keydown arrow up
+//2012/04/11 23:31:13 event: {-65362}	keyup arrow up
+//2012/04/11 23:31:16 event: {65364}	keydown arrow down
+//2012/04/11 23:31:16 event: {-65364}	keyup arrow down
+
+const arrowUp = 65362
+const arrowDown = 65364
+
+func rocketAdj(x int) {
+	rocketPos += x
+	if rocketPos < -rocketLimit {
+		rocketPos = -rocketLimit
+	}
+	if rocketPos > rocketLimit {
+		rocketPos = rocketLimit
+	}
+}
+
+func fire() {
+	// find a free flame and make it active
+	for i := 0; i < 10; i++ {
+		if !flames[i].active {
+			flames[i].active = true
+			flames[i].where.X = 10
+			flames[i].where.Y = rocketPos
+			break
+		}
+	}
+}
+
+var debounce int
+
 func processEvent(ch <-chan interface{}) {
 	for {
 		var ev interface{}
-		closed := false
+		ok := false
 		select {
-		case ev, closed = <-ch:
-			if !closed {
+		case ev, ok = <-ch:
+			if !ok {
 				log.Fatal("X display closed.")
 			}
-			// ok, continue
+			switch e := ev.(type) {
+			default:
+				log.Printf("event: %T", ev)
+			case ui.MouseEvent:
+				y := origin.Y + rocketPos
+				if e.Loc.Y < y {
+					rocketAdj(-7)
+				} else {
+					rocketAdj(7)
+				}
+				if debounce < 0 && e.Buttons&1 != 0 {
+					fire()
+					debounce = 5
+				}
+				debounce--
+			case ui.KeyEvent:
+				//log.Printf("key: %v", e.Key)
+				switch e.Key {
+				case arrowUp:
+					rocketAdj(-7)
+				case arrowDown:
+					rocketAdj(7)
+				case 'q':
+					log.Fatal("Exit.")
+				case ' ':
+					fire()
+				}
+			}
 		default:
 			// no events
 			return
@@ -86,6 +159,29 @@ func processEvent(ch <-chan interface{}) {
 }
 
 func main() {
+	flag.Parse()
+
+	var rocket image.Image
+	rfile, err := os.Open("rocket.png")
+	if err == nil {
+		rocket, err = png.Decode(rfile)
+	}
+	if err != nil {
+		log.Fatal("Cannot open rocket.png:", err)
+	}
+	rfile.Close()
+	rocketHeight := rocket.Bounds().Dy()
+
+	var flame image.Image
+	ffile, err := os.Open("flame.png")
+	if err == nil {
+		flame, err = png.Decode(ffile)
+	}
+	if err != nil {
+		log.Fatal("Cannot open flame.png:", err)
+	}
+	ffile.Close()
+
 	urls := make(chan string, 4)
 	imgReady := make(chan *image.Image, 4)
 
@@ -104,8 +200,6 @@ func main() {
 	//	log.Print("one tile is sized ", img.Bounds())
 	//	log.Print("the screen is sized ", screen.Bounds())
 
-	tileHeight := 256
-	tileWidth := 256
 	numTiles := screen.Bounds().Dx()/tileWidth + 2
 	tileStrip := image.NewRGBA(image.Rect(0, 0, numTiles*tileWidth, tileHeight))
 
@@ -116,11 +210,26 @@ func main() {
 		draw.Draw(tileStrip, image.Rect(i*tileWidth, 0, i*tileWidth+tileWidth, tileHeight), img, image.ZP, draw.Over)
 	}
 
+	rocketLimit = tileHeight/2 - rocketHeight/2
 	topBlack := (screen.Bounds().Dy() - tileHeight) / 2
+	origin = image.Pt(10, topBlack+tileHeight/2-rocketHeight/2)
 	for {
-		for x := 0; x < tileWidth; x += 15 {
+		for x := 0; x < tileWidth; x += 7 {
 			then := time.Now()
 			draw.Draw(screen, image.Rect(0, topBlack, screen.Bounds().Dx(), topBlack+tileHeight), tileStrip, image.Pt(x, 0), draw.Over)
+			if *game {
+				draw.Draw(screen, image.Rect(10, topBlack+tileHeight/2-rocketHeight/2+rocketPos, screen.Bounds().Dx(), topBlack+tileHeight), rocket, image.ZP, draw.Over)
+				for i := 0; i < 10; i++ {
+					if flames[i].active {
+						flames[i].where.X += 40
+						if flames[i].where.X > screen.Bounds().Dx() {
+							flames[i].active = false
+						}
+						draw.Draw(screen, image.Rectangle{flames[i].where.Add(origin).Add(image.Pt(0, rocketHeight/2)), screen.Bounds().Size()}, flame, image.Pt(0, 0), draw.Over)
+					}
+				}
+			}
+
 			now := time.Now()
 			frameTime := now.Sub(then)
 
@@ -140,3 +249,5 @@ func main() {
 		draw.Draw(tileStrip, image.Rect((numTiles-1)*tileWidth, 0, numTiles*tileWidth, tileHeight), img, image.ZP, draw.Over)
 	}
 }
+
+// vim:ts=2
