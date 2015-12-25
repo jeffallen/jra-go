1cc6519f6ca616e0273fb71a68480798b8821d5d
Jeff R. Allen <jra@nella.org>
2015-12-25 23:13:11 +0600

    Refuse connections that are not HTTP/2.

--- cmd/h2post/main.go
+++ cmd/h2post/main.go
@@ -3,23 +3,40 @@ package main
 import (
 	"bytes"
 	"crypto/tls"
+	"errors"
 	"flag"
 	"io"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"strings"
 )
 
-func main() {
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-		},
+var insecure = flag.Bool("insecure", false, "allow insecure TLS connections?")
+
+// A DialTLS that returns error if the server won't agree to do HTTP/2.
+func dialTLSonlyHTTP2(network, addr string) (net.Conn, error) {
+	cfg := &tls.Config{
+		InsecureSkipVerify: *insecure,
+		NextProtos:         []string{"h2"},
 	}
+
+	c, err := tls.Dial(network, addr, cfg)
+	if err != nil {
+		return nil, err
+	}
+	st := c.ConnectionState()
+	if st.NegotiatedProtocol != "h2" {
+		return nil, errors.New("server does not support HTTP/2")
+	}
+	return c, nil
+}
+
+func main() {
 	flag.Parse()
+
 	url := flag.Arg(0)
 	log.Print("fetching ", url)
 
@@ -28,14 +45,23 @@ func main() {
 		log.Fatal(err)
 	}
 	body := bytes.NewBuffer(in)
+
+	client := &http.Client{Transport: &http.Transport{DialTLS: dialTLSonlyHTTP2}}
 	resp, err := client.Post(url, "application/binary", body)
 	if err != nil {
-		log.Print(err)
-		return
+		log.Fatal(err)
 	}
+
+	log.Print("Server was:", resp.Header["Server"])
+	log.Printf("tls.ConnectionState was: %#v", resp.TLS)
+
+	if resp.Proto != "HTTP/2.0" {
+		log.Fatalf("Expecting proto HTTP/2.0, got %#v", resp.Proto)
+	}
+
 	if resp.StatusCode != 200 {
 		in, _ := ioutil.ReadAll(resp.Body)
-		log.Fatalf("status: %v (%v)", resp.StatusCode, string(in))
+		log.Fatalf("status: %v (%v)", resp.StatusCode, strings.Replace(string(in[0:100]), "\n", "", -1))
 	}
 	io.Copy(os.Stdout, resp.Body)
 	resp.Body.Close()
